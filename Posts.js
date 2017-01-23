cd017b0d4299277d1ab9f84f307616d83e148009
singheshadev <lanetteam.yasht@gmail.com>
2017-01-23 17:11:00 +0530

    jwt and relational db implementation

---
+++ Posts.js
@@ -0,0 +1,11 @@
+var mongoose=require('mongoose');
+
+var PostSchema=new mongoose.Schema({
+    title:String,
+    postedBy:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    }
+});
+module.exports=mongoose.model("Post",PostSchema);
+
